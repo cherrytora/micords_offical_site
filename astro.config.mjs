f6892f4990e0cecc34e3dadc9b9c36f01bdc45f2
cherrytora <yiren513@gmail.com>
2026-04-20 23:17:31 +0800

    修改config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://cherrytora.github.io/micords_offical_site',
+  site: 'https://cherrytora.github.io',
   base: '/micords_offical_site',
   integrations: [tailwind(), sitemap()],
 });
