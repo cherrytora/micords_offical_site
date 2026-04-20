52db5df594d58735f25ec52a83b6c5d1787cc375
Emma Wang <emmawang@ghtinc.com>
2026-04-20 15:34:39 +0800

    修改sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://cherrytora.github.io',
+  site: 'https://cherrytora.github.io/micords_offical_site',
   base: '/micords_offical_site',
   integrations: [tailwind(), sitemap()],
 });
